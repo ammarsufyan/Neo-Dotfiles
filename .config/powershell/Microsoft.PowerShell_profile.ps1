91f3dad0e428040eec59d6b84cb525248f0c711c
Ammar Sufyan <63483228+neouranos@users.noreply.github.com>
2023-01-29 11:32:43 +0700

    add powershell

---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Start OMP
+$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json" 
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+# Change icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadlineKeyHandler -Key Ctrl-Spacebar -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
